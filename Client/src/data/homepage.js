757beb240f7adc86f95dc9803b00edf5964f4023
Aritro Saha <titanssuperior@gmail.com>
2023-09-23 08:59:18 +0530

    corrected spelling

--- Client/src/data/homepage.js
+++ Client/src/data/homepage.js
@@ -1,57 +1,67 @@
 const tracks = [
-    {
-        id: 1,
-        title: "Prediction",
-        description: "Find out how likely your child is to drop-out of school.",
-        image: "https://cloud9.shauryasoft.com/media/contentpage_124_0_50.jpg?637530549829935635",
-        path: "/predict",
-    },
-    {
-        id: 2,
-        title: "Forum",
-        description: "Disucuss your problems and get help anonymously.",
-        image: "https://assets.weforum.org/article/image/nQPJNvyv_mGjtmr9m-vwPiT8QQJt2S1pgbXRpctea5Y.jpg",
-        path: "/comingsoon",
-    },
-    {
-        id: 3,
-        title: "Free Resources",
-        description: "Resources that help you study online without any cost.",
-        image: "https://blogassets.leverageedu.com/blog/wp-content/uploads/2019/10/23170101/List-of-Professional-Courses-after-Graduation.gif",
-        path: "/comingsoon",
-    },
-    {
-        id: 4,
-        title: "Donation",
-        description: "Donate for the needy children so that they can continue education.",
-        image: "https://images.onlymyhealth.com/imported/images/2022/August/13_Aug_2022/main-organdonation.jpg",
-        path: "/comingsoon",
-    }
-]
+  {
+    id: 1,
+    title: "Prediction",
+    description: "Find out how likely your child is to drop-out of school.",
+    image:
+      "https://cloud9.shauryasoft.com/media/contentpage_124_0_50.jpg?637530549829935635",
+    path: "/predict",
+  },
+  {
+    id: 2,
+    title: "Forum",
+    description: "Disucuss your problems and get help anonymously.",
+    image:
+      "https://assets.weforum.org/article/image/nQPJNvyv_mGjtmr9m-vwPiT8QQJt2S1pgbXRpctea5Y.jpg",
+    path: "/comingsoon",
+  },
+  {
+    id: 3,
+    title: "Free Resources",
+    description: "Resources that help you study online without any cost.",
+    image:
+      "https://blogassets.leverageedu.com/blog/wp-content/uploads/2019/10/23170101/List-of-Professional-Courses-after-Graduation.gif",
+    path: "/comingsoon",
+  },
+  {
+    id: 4,
+    title: "Donation",
+    description:
+      "Donate for the needy children so that they can continue education.",
+    image:
+      "https://images.onlymyhealth.com/imported/images/2022/August/13_Aug_2022/main-organdonation.jpg",
+    path: "/comingsoon",
+  },
+];
 
 const comments = [
-    {
-        id: 1,
-        name: "Ronoroa Zoro",
-        designation:"Lost Soul",
-        comment: "I wanted to reach the Wano Island but ended up here but trust me this is worth it",
-        image: "https://i.pinimg.com/originals/76/3f/40/763f40f9d25100d688e71d91e03ad07a.jpg",
-    },
-    {
-        id: 2,
-        name: "Nico Robin",
-        designation:"President of Baroque Works",
-        comment: "With the power of my devil fruit I am willing to reach out for help to every single needy children and this website is the best way to do it",
-        image: "https://stickerswiki.ams3.cdn.digitaloceanspaces.com/NicoRobin_ByTBB/1787583.512.webp",
-    },
-    {
-        id: 3,
-        name: "Monkey D. Luffy",
-        designation:"Captian of Straw Hats",
-        comment: "I am the future king of pirates and I will make sure that every single child gets the education they deserve and this website is the first step towards it",
-        image: "https://www.asiamediajournal.com/wp-content/uploads/2022/11/Free-One-Piece-Monkey-D.-Luffy-PFP.jpg",
-    },
-]
+  {
+    id: 1,
+    name: "Ronoroa Zoro",
+    designation: "Lost Soul",
+    comment:
+      "I wanted to reach the Wano Island but ended up here but trust me this is worth it",
+    image:
+      "https://i.pinimg.com/originals/76/3f/40/763f40f9d25100d688e71d91e03ad07a.jpg",
+  },
+  {
+    id: 2,
+    name: "Nico Robin",
+    designation: "President of Baroque Works",
+    comment:
+      "With the power of my devil fruit I am willing to reach out for help to every single needy children and this website is the best way to do it",
+    image:
+      "https://stickerswiki.ams3.cdn.digitaloceanspaces.com/NicoRobin_ByTBB/1787583.512.webp",
+  },
+  {
+    id: 3,
+    name: "Monkey D. Luffy",
+    designation: "Captain of Straw Hats",
+    comment:
+      "I am the future king of pirates and I will make sure that every single child gets the education they deserve and this website is the first step towards it",
+    image:
+      "https://www.asiamediajournal.com/wp-content/uploads/2022/11/Free-One-Piece-Monkey-D.-Luffy-PFP.jpg",
+  },
+];
 
-
-export {tracks,comments};
+export { tracks, comments };
